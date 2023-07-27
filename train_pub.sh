e9691d121ce93d19ddd392b05a754b9c828a34e1
lupoalberto98 <alberto.bassi98@gmail.com>
2023-07-27 10:36:12 +0200

    up

--- train_pub.sh
+++ train_pub.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
-
+#SBATCH --job-name=pub_lstm
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=3
+#SBATCH --time=24:00:00
+#SBATCH --partition=earth-4
+#SBATCH --constraint=rhel8
+#SBATCH --gres=gpu:a100:1
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END,FAIL     
+#SBATCH --output=pub_lstm.out
+#SBATCH --error=pub_lstm.err
 nsplits=12
 nseeds=10
 firstseed=300
+gpu=0
 
-gpucount=-1
+
 for (( seed = $firstseed ; seed < $((nseeds+$firstseed)) ; seed++ )); do
 
   python3 main.py --n_splits=$nsplits --seed=$seed create_splits 
   wait
 
   for ((split = 0 ; split < $nsplits ; split++ )); do  
-
-    gpucount=$(($gpucount + 1))
-    gpu=$(($gpucount % 3))
-    echo $seed $gpucount $gpu
+    
+    echo $seed $gpu
 
     if [ "$1" = "lstm" ]
     then
 
-      outfile="reports/pub_lstm_extended_nldas.$seed.$split.out"
-      python3 main.py --gpu=$gpu --no_static=False --concat_static=True --split=$split --split_file="data/kfold_splits_seed$seed.p" train > $outfile &
+      outfile="reports/pub_lstm_nostat_extended_nldas.$seed.$split.out"
+      python3 main.py --gpu=$gpu --no_static=True --split=$split --split_file="data/kfold_splits_seed$seed.p" train > $outfile &
 
     else
       echo bad model choice
