f949d2570c578b60752aedcdf6daee8ca729b99b
lupoalberto98 <alberto.bassi98@gmail.com>
2023-07-27 11:03:31 +0200

    up

--- train_pub.sh
+++ train_pub.sh
@@ -31,7 +31,7 @@ for (( seed = $firstseed ; seed < $((nseeds+$firstseed)) ; seed++ )); do
   python3 main.py --n_splits=$nsplits --seed=$seed create_splits 
   wait
 
-  for ((split = 0 ; split < $nsplits ; split++ )); do  
+  for ((split = 0 ; split < 6; split++ )); do  
     
     echo $seed $gpu
 
