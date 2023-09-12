ad375f090e510ea892b2efebbc79313ef56a4358
Alberto Bassi <alberto.bassi.3@studenti.unipd.it>
2023-09-12 20:51:41 +0200

    Added analysis sh file

---
+++ run_pub.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=pub_lstm
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=3
+#SBATCH --time=5:00:00
+#SBATCH --partition=earth-4
+#SBATCH --constraint=rhel8
+#SBATCH --gres=gpu:a100:1
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END,FAIL     
+#SBATCH --output=pub_lstm.out
+#SBATCH --error=pub_lstm.err
+
+module load gcc/9.4.0-pe5.34 miniconda3/4.12.0 lsfm-init-miniconda/1.0.0	# comment to run on your machine
+
+
+# the above code is meant to run on ZAHW cluster in Zurich. Modify it to use in your machine/cluster. 
+# from here onwards you can run everywhere
+
+conda activate hydro # comment to run on your machine
+
+python run_pub.py LSTM $1 $2 # $1=experiment $2=gpu
